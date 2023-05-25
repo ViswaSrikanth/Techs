acc0cfcb2e5db5c50ed3beaf270716ca76c3ca12
Viswa Srikanth <v.viswasrikanth@gmail.com>
2023-05-25 08:43:33 +0530

    Deleted

--- ledgingapp/src/app/constants/app.constants.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AppConstants } from './app.constants';
-
-describe('App', () => {
-  it('should create an instance', () => {
-    expect(new App()).toBeTruthy();
-  });
-});
--- ledgingapp/src/app/transactions/mock-transactions.ts
+++
@@ -1,9 +0,0 @@
-import { Transaction } from './transaction';
-
-export const TRANSACTIONS: Transaction[] = [
-  { id: 11, transaction: 'Fund', from_acc: 'Mr. Nice', to_acc: '', description: '' },
-  { id: 12, transaction: '', from_acc: 'Narco', to_acc: '', description: '' },
-  { from_acc: 'Bombasto', id: 13, transaction: '', to_acc: '', description: '' },
-  { id: 14, transaction: '', from_acc: 'Celeritas', to_acc: '', description: '' },
-  { transaction: '', id: 15, from_acc: 'Magneta', to_acc: '', description: '' }
-];
